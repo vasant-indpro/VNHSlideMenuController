221409303e1470aff4a8a306216cffd364de8c73
Vasant Hugar <vasant.hugar@indpro.se>
2017-10-31 10:53:27 +0530

    Initial Commit

---
+++ VNHSlideMenuController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VNHSlideMenuController
+//
+//  Created by Vasant Hugar on 31/10/17.
+//  Copyright © 2017 Vasant Hugar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
